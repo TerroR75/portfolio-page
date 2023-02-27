41720ad5fa8d79579a722590be36e20f5a161902
TerroR75 <kamilsulimierski@gmail.com>
2023-02-27 18:57:23 +0100

    Added NavBar.jsx and NavBar.scss
    NavBar component with navigation functionality and its styling file with responsivity

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,22 @@
+function NavBar() {
+  return (
+    <nav className='main-navbar'>
+      <ul>
+        <a href='#entry-section'>
+          <li>Home</li>
+        </a>
+        <a href='#about-section'>
+          <li>About</li>
+        </a>
+        <a href='#project-section'>
+          <li>Projects</li>
+        </a>
+        <a href='#contact-section'>
+          <li>Contact</li>
+        </a>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
