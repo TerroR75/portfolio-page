fe1814b282af9656bb80d89112d6eb2241d08619
TerroR75 <kamilsulimierski@gmail.com>
2023-02-27 18:54:24 +0100

    Added id for NavBar, bugfix for "className" attribute
    ID required for the NavBar and changed "class" to "className" which prevents errors in the console

--- src/Components/EntrySection.jsx
+++ src/Components/EntrySection.jsx
@@ -5,7 +5,7 @@ function EntrySection() {
   const [count, setCount] = useState(0);
 
   return (
-    <section className='Entry-Section'>
+    <section id='entry-section' className='Entry-Section'>
       <div className='name typewriter'>
         <h1>Kamil Sulimierski</h1>
         <span className='entry-description'>
@@ -13,10 +13,10 @@ function EntrySection() {
         </span>
         <div className='links'>
           <a href='https://github.com/TerroR75' target='_blank'>
-            <i class='devicon-github-original'></i>
+            <i className='devicon-github-original'></i>
           </a>
           <a href='https://www.linkedin.com/in/kamil-sulimierski-5610531b5/' target='_blank'>
-            <i class='devicon-linkedin-plain'></i>
+            <i className='devicon-linkedin-plain'></i>
           </a>
         </div>
       </div>
