6e78d992f5fc7b9c09e8b77c62df2e824d329dd9
TerroR75 <kamilsulimierski@gmail.com>
2023-03-04 16:23:00 +0100

    Added data for stack technologies
    Technologies split into different categories showcasing what i know and my plans

---
+++ src/data/techstacklist.js
@@ -0,0 +1,124 @@
+const frontend = [
+  {
+    id: '00',
+    name: 'JavaScript',
+    level: 'Junior',
+    iconClass: 'devicon-javascript-plain colored',
+  },
+  {
+    id: '01',
+    name: 'CSS3',
+    level: 'Junior',
+    iconClass: 'devicon-css3-plain colored',
+  },
+  {
+    id: '02',
+    name: 'HTML5',
+    level: 'Junior',
+    iconClass: 'devicon-html5-plain colored',
+  },
+  {
+    id: '03',
+    name: 'React',
+    level: 'Basic',
+    iconClass: 'devicon-react-original colored',
+  },
+];
+
+const backend = [
+  //   {
+  //     id: '11',
+  //     name: 'PHP',
+  //     level: 'Basic',
+  //     iconClass: 'devicon-php-plain colored',
+  //   },
+  {
+    id: '12',
+    name: 'NodeJS',
+    level: 'Basic',
+    iconClass: 'devicon-nodejs-plain colored',
+  },
+];
+
+const experience = [
+  {
+    id: '21',
+    name: 'Firebase',
+    level: 'Basic',
+    iconClass: 'devicon-firebase-plain colored',
+  },
+  {
+    id: '22',
+    name: 'Express',
+    level: 'Basic',
+    iconClass: 'devicon-express-original',
+  },
+  {
+    id: '23',
+    name: 'Bootstrap',
+    level: 'Basic',
+    iconClass: 'devicon-bootstrap-plain colored',
+  },
+  {
+    id: '24',
+    name: 'MongoDB',
+    level: 'Basic',
+    iconClass: 'devicon-mongodb-plain colored',
+  },
+  {
+    id: '25',
+    name: 'Webpack',
+    level: 'Basic',
+    iconClass: 'devicon-webpack-plain colored',
+  },
+];
+
+const learning = [
+  {
+    id: '31',
+    name: 'PHP',
+    level: 'Basic',
+    iconClass: 'devicon-php-plain colored',
+  },
+  {
+    id: '32',
+    name: 'TypeScript',
+    level: 'Basic',
+    iconClass: 'devicon-typescript-plain colored',
+  },
+  {
+    id: '33',
+    name: 'MySQL',
+    level: 'Basic',
+    iconClass: 'devicon-mysql-plain colored',
+  },
+  {
+    id: '34',
+    name: 'Jest',
+    level: 'Basic',
+    iconClass: 'devicon-jest-plain colored',
+  },
+];
+
+const futurePlans = [
+  {
+    id: '41',
+    name: 'C#',
+    level: 'Basic',
+    iconClass: 'devicon-csharp-plain colored',
+  },
+  {
+    id: '42',
+    name: 'Python',
+    level: 'Basic',
+    iconClass: 'devicon-python-plain colored',
+  },
+  {
+    id: '43',
+    name: 'C++',
+    level: 'Basic',
+    iconClass: 'devicon-cplusplus-plain colored',
+  },
+];
+
+export { frontend, backend, experience, learning, futurePlans };
