49408abd4d9697e90d4cb9435b7249f9db3874e4
TerroR75 <kamilsulimierski@gmail.com>
2023-03-04 13:38:00 +0100

    Assets folder restructure

--- src/Components/about/TechStack.jsx
+++ src/Components/about/TechStack.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function TechStack() {
   return (
     <div className='tech-stack'>
-      <img className='gear-512' src='src\assets\gears\cogwheel-svgrepo-com.svg' alt='Cogwheel'></img>
-      <img className='gear-512' src='src\assets\gears\cogwheel-svgrepo-com.svg' alt='Cogwheel'></img>
+      <img className='gear-512' src='src\assets\cogwheel-svgrepo-com.svg' alt='Cogwheel'></img>
+      <img className='gear-512' src='src\assets\cogwheel-svgrepo-com.svg' alt='Cogwheel'></img>
     </div>
   );
 }
