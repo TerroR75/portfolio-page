f4972bbcc47fb4b7d78197c930f7bf8ca2bf29a8
Kam Sul <kamilsulimierski@gmail.com>
2023-06-20 17:01:36 +0200

    Netlify env varaibles

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import { gql, GraphQLClient } from "graphql-request";
 
-const graphqlAPI = `https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/${process.env.VITE_GRAPHCMS_PUBLIC_ENDPOINT}/master`;
+const graphqlAPI = `https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/${
+  import.meta.env.VITE_GRAPHCMS_PUBLIC_ENDPOINT
+}/master`;
 const graphcms = new GraphQLClient(graphqlAPI);
 
 export const getAllTechs = async () => {
